2d3b837baa7ebc62b5879b9280f4d5f2e686b6a0
Martin Studer, fluxlabs ag <martin@fluxlabs.ch>
2022-09-30 14:26:49 +0200

    7.13-2 - use mariadb:10.7-focal

--- build/ilias7/build.sh
+++ build/ilias7/build.sh
@@ -7,8 +7,17 @@ CRON_BUILD=fluxms/flux-ilias-cron
 ILSERVER_BUILD=fluxms/flux-ilias-ilserver
 DATABASE_BUILD=fluxms/flux-ilias-database
 
+docker rmi $ILIAS_BUILD:$MAJOR_VERSION
 docker build --target ilias -t $ILIAS_BUILD:$MAJOR_VERSION .
+
+docker rmi $NGINX_BUILD:$MAJOR_VERSION
 docker build --target nginx -t $NGINX_BUILD:$MAJOR_VERSION .
+
+docker rmi $CRON_BUILD:$MAJOR_VERSION
 docker build --target cron -t $CRON_BUILD:$MAJOR_VERSION .
+
+docker rmi $ILSERVER_BUILD:$MAJOR_VERSION
 docker build --target ilserver -t $ILSERVER_BUILD:$MAJOR_VERSION .
+
+docker rmi $DATABASE_BUILD:$MAJOR_VERSION
 docker build --target database -t $DATABASE_BUILD:$MAJOR_VERSION .
